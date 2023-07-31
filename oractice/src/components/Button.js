8146ffc5d709046827a593df7db78c655009f1e9
YukihiraDeve <valentinbourpro@gmail.com>
2023-08-01 00:41:34 +0200

    first button

---
+++ oractice/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Button = () => {
+    return (
+           <button className="button-back md:text-lg lg:text-xl">Bouton</button>
+    );
+};
+
+export default Button;
---
+++ oractice/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Button from '../components/Button';
+
+const Home = () => {
+    return (
+    
+        <div>
+          <Header />
+          <Button />
+        </div>
+
+
+
+    );
+};
+
+export default Home;
