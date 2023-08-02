afdaefa48924b4727a792bee48d4a5a1c63260d3
YukihiraDeve <valentinbourpro@gmail.com>
2023-08-03 01:09:25 +0200

    light bouge + responsive

--- oractice/src/components/FirstZone.js
+++ oractice/src/components/FirstZone.js
@@ -34,11 +34,13 @@ const FirstZone = () => {
       </p>
     </div>
 
-      <LightEffect color="#FF5733" top="10px" left="50px" />
-      <LightEffect color="#B8F868" top="800px" left="200px" />
-      <LightEffect color="#F86868" top="900px" left="900px" />
-      <LightEffect color="#68D5F8" top="100px" left="1000px" />
-      <LightEffect color="#A768F8" top="100px" left="2000px" />
+      <LightEffect color="#FF5733" top="10vh" left="10vw" />
+      <LightEffect color="#B8F868" top="49vh" left="25vh" />
+      <LightEffect color="#F86868" top="64vh" left="45vh" />
+      <LightEffect color="#68D5F8" top="20vh" left="70vh" />
+      <LightEffect color="#A768F8" top="0vh" left="90vh" />
+      <LightEffect color="#A43265" top="90vh" left="0vh" />
+      <LightEffect color="#FFAFF0" top="0vh" left="50vh" />
 
       <div
         className="image-section w-full md:w-1/2 h-[50vh] md:h-[100vh] flex-shrink-0 bg-no-repeat bg-center m-0 p-0"
--- oractice/src/components/LightEffect.js
+++ oractice/src/components/LightEffect.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+const LightEffect = ({ color = '#B8F868', top, left }) => {
+  const [position, setPosition] = useState({ top: `${top}vh`, left: `${left}vw` });
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTop = parseFloat(position.top);
+      const currentLeft = parseFloat(position.left);
+      const newTopValue = Math.min(Math.max(currentTop + (Math.random() * 10 - 5), 0), 90); // Limite entre 0 et 90
+      const newLeftValue = Math.min(Math.max(currentLeft + (Math.random() * 10 - 5), 0), 90); // Limite entre 0 et 90
+      const newTop = newTopValue + 'vh'; 
+      const newLeft = newLeftValue + 'vw';
+      setPosition({ top: newTop, left: newLeft });
+    }, 50); // Changer de position toutes les secondes
 
-const LightEffect = ({ color = '#B8F868', top, left, right, bottom }) => {
-    return (
-      <div
-        className="rounded-full border border-black absolute"
-        style={{
-          backgroundColor: color,
-          filter: 'blur(62px)',
-          width: '27rem',
-          height: '27rem',
-          opacity: '0.19',
-          top: top,
-          left: left,
-          right: right,
-          bottom: bottom,
-        }}
-      ></div>
-    );
-  };
+    return () => clearInterval(interval);
+  }, [position]);
 
-  
-  
-  
+  return (
+    <div
+      className="rounded-full border border-black absolute transition-all duration-1000 ease-in-out"
+      style={{
+        backgroundColor: color,
+        filter: 'blur(3vw)',
+        width: '10vw',
+        height: '10vw',
+        opacity: '0.9',
+        ...position,
+      }}
+    ></div>
+  );
+};
+
 export default LightEffect;
